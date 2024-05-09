9fee4bea2052892ddf5dea4ee526d69ac61e0cb7
Dawid Sołek <dsolek108@gmail.com>
2024-05-09 20:31:19 +0200

    scripts.js fix and number of institutions/donations view

--- charity_donation_app/views.py
+++ charity_donation_app/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.views import View
+from charity_donation_app.models import Donation, Institution
 
 
 class LandingPageView(View):
 
     def get(self, request):
-        return render(request, 'index.html')
+        donations = Donation.objects.all()
+        institutions = Institution.objects.all()
+        bags_donated = sum(donation.quantity for donation in donations)
+        institutions_helped = len(institutions)
+        return render(request, 'index.html', {'bags_donated': bags_donated,
+                                              'institutions_helped': institutions_helped})
 
 
 class AddDonationView(View):
